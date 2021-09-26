29b01ec457779b8602f3e620661e996740e11d2d
Sadiq O <sadiqomar890@gmail.com>
2021-09-26 11:56:14 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const App = () => {
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  const fetchMovieHandler = async () => {
+    try {
+      setIsLoading(true);
+      const response = await fetch("https://swapi.dev/api/films");
+
+      const data = await response.json();
+      const results = await data.results;
+      setMovies(results);
+      setIsLoading(false);
+    } catch (error) {
+      setError(error.message);
+      setIsLoading(false);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-fluid mt-3">
+      <h1 className="text-center">Movies App</h1>
+      <div className="w-50 m-auto p-2">
+        <button className="btn-primary btn" onClick={fetchMovieHandler}>
+          Fetch movies
+        </button>
+      </div>
+      <h5 className="text-center m-auto rounded p-2 w-50 ">Movies List</h5>
+      {isLoading && <p className="text-center">Loading...</p>}
+      {!isLoading && (
+        <div className="card" style={{ margin: "auto", width: "50%" }}>
+          {movies.map((movie) => (
+            <div className="card-header">
+              <h4> {movie.title}</h4>
+              <p>Director : {movie.director}</p>
+              <p>Release Date : {movie.release_date} </p>
+            </div>
+          ))}
+
+          {error.trim().length > 1 && <p className="text-center"> {error}</p>}
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
